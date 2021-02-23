3dd989811790f7dfe4dfad58cc758cbd6abd4904
Joseph <joseph.serido@gmail.com>
2021-02-23 06:16:37 -0500

    cleanup

--- app.js
+++ app.js
@@ -10,11 +10,8 @@ const loader = new THREE.TextureLoader();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const cubes = createCubes(40);
-cubes.forEach(cube => scene.add(cube));
-
-const light = createLight();
-scene.add(light);
+const flyingWindows = createFlyingWindows(40);
+flyingWindows.forEach(flyingWindow => scene.add(flyingWindow));
 
 camera.position.z = 2;
 
@@ -25,24 +22,26 @@ function animate() {
   requestAnimationFrame(animate);
   camera.position.z -= 0.7;
 
-  cubes.forEach(cube => {
-    if (camera.position.z < cube.position.z) {
-      resetCube(cube, camera);
+  flyingWindows.forEach(flyingWindow => {
+    if (camera.position.z < flyingWindow.position.z) {
+      resetFlyingWindow(flyingWindow, camera);
     }
   });
 
   renderer.render(scene, camera);
 }
 
-function createCubes(qty) {
+function createFlyingWindows(qty) {
+  // width and height are based on the aspect of the Windows logo image
   const width = 1.235;
   const height = 1;
+  // arbitrary depth to make it appear flat enough
   const depth = 0.01;
 
   const geometry = new THREE.BoxGeometry(width, height, depth);
-  const cubes = [];
+  const flyingWindows = [];
 
-  const windowsImageMaterial = new THREE.MeshBasicMaterial({
+  const windowImageMaterial = new THREE.MeshBasicMaterial({
     map: loader.load('window-filter.png'),
     transparent: true,
     side: THREE.FrontSide
@@ -51,7 +50,8 @@ function createCubes(qty) {
   for (let i = 0; i < qty; i++) {
     const color = getRandomColor();
 
-    const colorMaterial = new THREE.MeshPhongMaterial({
+    // put color on both sides so transparent front can show color from inside box
+    const colorMaterial = new THREE.MeshBasicMaterial({
       color,
       side: THREE.DoubleSide,
     });
@@ -61,57 +61,58 @@ function createCubes(qty) {
       colorMaterial,
       colorMaterial,
       colorMaterial,
-      windowsImageMaterial,
+      windowImageMaterial, // side facing camera
       colorMaterial,
     ];
 
-    const cube = new THREE.Mesh(geometry, materials);
-    cube.position.z = -1 * (Math.floor(Math.random() * 50) - 100);
-    cube.position.x = Math.floor(Math.random() * 2 * Math.abs(cube.position.z)) + 0.7;
-    cube.position.y = Math.floor(Math.random() * 2 * Math.abs(cube.position.z)) + 0.7;
+    const flyingWindow = new THREE.Mesh(geometry, materials);
 
+    // randomly position z away from camera
+    flyingWindow.position.z = -1 * (Math.floor(Math.random() * 50) - 100);
+
+    // based on how far away the flyingWindow is, push its X and Y position
+    // add 0.7 at the end to keep it away from the Z axis slightly
+    flyingWindow.position.x = Math.floor(Math.random() * 2 * Math.abs(flyingWindow.position.z)) + 0.7;
+    flyingWindow.position.y = Math.floor(Math.random() * 2 * Math.abs(flyingWindow.position.z)) + 0.7;
+
+    // randomly place it in one of the 4 quadrants
     if (Math.random() > 0.5) {
-      cube.position.x *= -1;
+      flyingWindow.position.x *= -1;
     }
 
     if (Math.random() > 0.5) {
-      cube.position.y *= -1;
+      flyingWindow.position.y *= -1;
     }
 
-    cubes.push(cube);
+    flyingWindows.push(flyingWindow);
   }
 
-  return cubes;
+  return flyingWindows;
 }
 
-function resetCube(cube, camera) {
-  cube.position.z = camera.position.z - Math.floor(Math.random() * 30) - 50;
-  const distance = Math.abs(camera.position.z - cube.position.z);
-  cube.position.x = Math.floor(Math.random() * (2 + distance / 2)) + 0.5;
-  cube.position.y = Math.floor(Math.random() * (2 + distance / 2)) + 0.5;
+function resetFlyingWindow(flyingWindow, camera) {
+  // perform the same thing we did when we first set the flying windows, except this
+  // time use the camera's z position to determine where to place them on the z axis
+  flyingWindow.position.z = camera.position.z - Math.floor(Math.random() * 50) - 100;
+  const distance = Math.abs(camera.position.z - flyingWindow.position.z);
+  flyingWindow.position.x = Math.floor(Math.random() * (2 + distance / 2)) + 0.7;
+  flyingWindow.position.y = Math.floor(Math.random() * (2 + distance / 2)) + 0.7;
 
   if (Math.random() > 0.5) {
-    cube.position.x *= -1;
+    flyingWindow.position.x *= -1;
   }
 
   if (Math.random() > 0.5) {
-    cube.position.y *= -1;
+    flyingWindow.position.y *= -1;
   }
 
+  // reset the color with each reset
   const color = getRandomColor();
-  cube.material[0].color.setHex = color;
-  cube.material[1].color.setHex = color;
-  cube.material[2].color.setHex = color;
-  cube.material[3].color.setHex = color;
-  cube.material[5].color.setHex = color;
-}
-
-function createLight() {
-  const color = 0xFFFFFF;
-  const intensity = 1;
-  const light = new THREE.DirectionalLight(color, intensity);
-  light.position.set(-1, 2, 4);
-  return light;
+  flyingWindow.material[0].color.setHex = color;
+  flyingWindow.material[1].color.setHex = color;
+  flyingWindow.material[2].color.setHex = color;
+  flyingWindow.material[3].color.setHex = color;
+  flyingWindow.material[5].color.setHex = color;
 }
 
 function onWindowResize() {
